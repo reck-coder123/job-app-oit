476b32049861107058d7ef4237a0308577c2c2d1
reck-coder123 <105230070+reck-coder123@users.noreply.github.com>
2022-10-23 19:41:17 +0530

    Update db.js

--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,8 @@ const mongoose=require('mongoose');
 mongoose.connect("mongodb://localhost:27017/Jobs",{
     useNewUrlParser:true,
     useCreateIndex:true,
-    useUnifiedTopology:true
+    useUnifiedTopology:true,
+    useFindAndModify:false
 }).then(()=>{
     console.log('connection successfull')
 }).catch((e)=>{
